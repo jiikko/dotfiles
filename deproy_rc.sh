c36b5b943db6709cb5009a611d681b8443678b71
jiikko <n905i.1214@gmail.com>
2013-03-05 10:03:59 +0900

    add (dep and overwrite).sh

--- deproy_rc.sh
+++ deproy_rc.sh
@@ -1,3 +1,8 @@
 cp .bashrc ~/.bashrc
+
 cp .vimrc ~/.vimrc
+
+#プラグイン周りの持ってく
 #cp -r .vim ~/.vim
+
+cp ~/.screenrc .screenrc
--- overwrite_rc.sh
+++ overwrite_rc.sh
@@ -1,4 +1,7 @@
 # マシンの設定ファイルをリポジトリに上書きする
 cp ~/.bash_profile .bash_profile
 cp ~/.bashrc .bashrc
+
 cp ~/.vimrc .vimrc
+
+cp ~/.screenrc .screenrc
