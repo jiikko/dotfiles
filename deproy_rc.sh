4c9f2cc4b0facb58bb453dfc24695392655faa69
jiikko <n905i.1214@gmail.com>
2013-06-01 00:57:44 +0900

    Deleted deploy's sh

--- deproy_rc.sh
+++
@@ -1,8 +0,0 @@
-cp .bashrc ~/.bashrc
-
-cp .vimrc ~/.vimrc
-
-#プラグイン周りの持ってく
-#cp -r .vim ~/.vim
-
-cp .screenrc ~/.screenrc
--- overwrite_rc.sh
+++
@@ -1,8 +0,0 @@
-# マシンの設定ファイルをリポジトリに上書きする
-cp ~/.bash_profile .bash_profile
-cp ~/.bashrc .bashrc
-cp ~/.zshrc .zshrc
-
-cp ~/.vimrc .vimrc
-
-cp ~/.screenrc .screenrc
