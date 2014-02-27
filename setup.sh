107f092fd447cfcd7f9972ffb5a12e1170820e8d
jiikko <n905i.1214@gmail.com>
2014-02-27 11:26:29 +0900

    pryrcいれる

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-for file in bashrc bash_pofile gemrc screenrc vimrc zshrc rspec gitconfig gitignore_global
+for file in bashrc bash_pofile gemrc screenrc vimrc zshrc rspec gitconfig gitignore_global pryrc
 do
   echo 'making symlink' _$file '->' $HOME/.$file
   ln -s `pwd`/_$file $HOME/.$file
