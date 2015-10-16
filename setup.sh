4356f43b58109a67ed7fb6406c5008670b65c59f
jiikko <n905i.1214@gmail.com>
2015-10-17 01:01:56 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # set rc limlink
-for file in bashrc bash_pofile gemrc screenrc vimrc zshrc rspec gitconfig gitignore_global pryrc zlogin
+# bash 使ってないので除外 bashrc bash_pofile
+for file in gemrc screenrc vimrc zshrc rspec gitconfig gitignore_global pryrc zlogin
 do
   echo 'making symlink' _$file '->' $HOME/.$file
-  ln -s `pwd`/_$file $HOME/.$file
+  ln -s -F `pwd`/_$file $HOME/.$file
 done
 
 
