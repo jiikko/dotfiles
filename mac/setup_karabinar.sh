e266ff5148d86b81255b37351ae3f571ff09c2f8
jiikko <n905i.1214@gmail.com>
2016-03-25 07:51:01 +0900

    update mac/setup_karabinar.sh

--- mac/setup_karabinar.sh
+++ mac/setup_karabinar.sh
@@ -7,11 +7,11 @@ git clone git@github.com:jiikko/dotfiles.git || git clone https://github.com/jii
 cd ~/dotfiles/mac
 sudo cp karabinar_private.xml ~/Library/Application\ Support/Karabiner/private.xml
 
-$cli set repeat.initial_wait 150
+$cli set repeat.initial_wait 216
 /bin/echo -n .
 $cli set private.space_to_command 1
 /bin/echo -n .
-$cli set repeat.wait 20
+$cli set repeat.wait 16
 /bin/echo -n .
 $cli set private.comannd_l_to_space 1
 /bin/echo -n .
@@ -19,17 +19,19 @@ $cli set hf_to_tag 1
 /bin/echo -n .
 $cli set private.menu_to_control 1
 /bin/echo -n .
-$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
-/bin/echo -n .
-$cli set parameter.mouse_key_scroll_not_natural_direction 1
-/bin/echo -n .
 $cli set private.comannd_r_to_space 1
 /bin/echo -n .
 $cli set remap.mouse_keys_mode_2 1
 /bin/echo -n .
+$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
+/bin/echo -n .
 $cli set private.control_to_shift 1
 /bin/echo -n .
 $cli set option.emacsmode_controlLeftbracket 1
+/bin/echo -n .
+$cli set remap.jis_command2eisuukana_prefer_command 1
+/bin/echo -n .
+$cli set remap.commandLcommandR2enter 1
 /bin/echo -n .
 $cli set private.shift_r_to_command 1
 /bin/echo -n .
