afa6214dbb44bcccdf8cd99654c26d8631650278
jiikko <n905i.1214@gmail.com>
2016-07-15 19:54:15 +0900

    fix ~

--- setup.sh
+++ setup.sh
@@ -3,12 +3,12 @@
 # set rc limlink
 for file in gemrc screenrc bashrc bash_profile vimrc zshrc rspec gitconfig gitignore_global pryrc zlogin
 do
-  echo 'making symlink' _$file '->' $HOME/.$file
-  ln -s -F `pwd`/_$file $HOME/.$file
+  echo 'making symlink' _$file '->' ~/.$file
+  ln -s -F `pwd`/_$file ~/.$file
 done
 
 # set dict symlink
-mkdir -p $HOME/.vim/dict
-ln -s -F $DOTFILE_FULLPATH/lib/vim/dict/javascript.dict $HOME/.vim/dict
-ln -s -F $DOTFILE_FULLPATH/lib/vim/dict/jquery.dict     $HOME/.vim/dict
-ln -s -F $DOTFILE_FULLPATH/lib/vim/dict/ruby2.1.0.dict  $HOME/.vim/dict
+mkdir -p ~/.vim/dict
+ln -s -F $DOTFILE_FULLPATH/lib/vim/dict/javascript.dict ~/.vim/dict
+ln -s -F $DOTFILE_FULLPATH/lib/vim/dict/jquery.dict     ~/.vim/dict
+ln -s -F $DOTFILE_FULLPATH/lib/vim/dict/ruby2.1.0.dict  ~/.vim/dict
