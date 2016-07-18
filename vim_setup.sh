ae7a33ef46ec7d3aa2b29bc3b4fb48939639606f
jiikko <n905i.1214@gmail.com>
2016-07-18 17:47:44 +0900

    fix vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-cp ~/.vimrc ~/.temp_vimrc
+cp ~/.vimrc ~/.original_vimrc
 
-$(cat << EOH > ~/.vimrc
+<< EOH > ~/.vimrc
 if 0 | endif
 if &compatible
   set nocompatible
@@ -13,16 +13,21 @@ call neobundle#begin(expand('~/.vim/bundle/'))
   NeoBundleFetch 'Shougo/neobundle.vim'
 call neobundle#end()
 filetype plugin indent on
+call feedkeys(" ")
 NeoBundleInstall
 EOH
-)
 vim -c q
 
 ruby << EOH > ~/.vimrc
-file = File.read File.expand_path("~/.vimrc")
+file = File.read File.expand_path("~/.original_vimrc")
 file =~ /" NEOBUNDLE_START(.*?)" NEOBUNDLE_END/m
-puts \$1
+puts <<-RUBY_EOH
+#{\$1}
+call feedkeys(" ")
+NeoBundleInstall
+RUBY_EOH
 EOH
 vim -c q
 
-cat ~/.temp_vimrc > ~/.vimrc
+cat ~/.original_vimrc > ~/.vimrc
+rm ~/.original_vimrc
