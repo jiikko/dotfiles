075996de77c092b829757ff282eb989afe8480c3
jiikko <n905i.1214@gmail.com>
2016-08-07 22:35:30 +0900

    add karabinar conf

--- mac/setup_karabinar.sh
+++ mac/setup_karabinar.sh
@@ -17,17 +17,7 @@ $cli set private.space_to_command 1
 /bin/echo -n .
 $cli set repeat.wait 16
 /bin/echo -n .
-$cli set private.comannd_l_to_space 1
-/bin/echo -n .
-$cli set hf_to_tag 1
-/bin/echo -n .
-$cli set private.menu_to_control 1
-/bin/echo -n .
 $cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
-/bin/echo -n .
-$cli set private.comannd_r_to_space 1
-/bin/echo -n .
-$cli set private.control_to_shift 1
 /bin/echo -n .
 $cli set option.emacsmode_controlLeftbracket 1
 /bin/echo -n .
@@ -39,16 +29,18 @@ $cli set remap.mouse_keys_mode_2 1
 /bin/echo -n .
 $cli set remap.commandLcommandR2enter 1
 /bin/echo -n .
-$cli set private.shift_r_to_command 1
-/bin/echo -n .
 $cli set option.emacsmode_controlPNBF 1
-/bin/echo -n .
-$cli set private.app_to_option 1
 /bin/echo -n .
 $cli set remap.controlJ2enter 1
 /bin/echo -n .
 $cli set option.emacsmode_controlI 1
 /bin/echo -n .
 $cli set option.jis_emacsmode_controlLeftbracket 1
+/bin/echo -n .
+$cli set private.caps_to_a_key_for_tron 1
+/bin/echo -n .
+$cli set private.eisuu_to_a_key_for_emacs_koyubi 1
+/bin/echo -n .
+$cli set private.kana_to_ctrl_for_emacs_koyubi 1
 /bin/echo -n .
 /bin/echo
