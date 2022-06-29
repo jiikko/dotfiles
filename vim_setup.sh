79cd3739a141e806ac2a57f9750bf96ae6cb77ac
jiikko <n905i.1214@gmail.com>
2022-06-29 18:32:19 +0900

    update vim script for neovim

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,33 +1,5 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
-cp ~/.vimrc ~/.original_vimrc
-
-<< EOH > ~/.vimrc
-if 0 | endif
-if &compatible
-  set nocompatible
-endif
-set runtimepath^=~/.vim/bundle/neobundle.vim/
-call neobundle#begin(expand('~/.vim/bundle/'))
-  NeoBundleFetch 'Shougo/neobundle.vim'
-call neobundle#end()
-filetype plugin indent on
-call feedkeys(" ")
-NeoBundleInstall
-EOH
-vim -c q
-
-ruby << EOH > ~/.vimrc
-file = File.open(File.expand_path("~/.original_vimrc"), 'r:utf-8').read
-file =~ /" NEOBUNDLE_START(.*?)" NEOBUNDLE_END/m
-puts <<-RUBY_EOH
-#{\$1}
-call feedkeys(" ")
-NeoBundleInstall
-RUBY_EOH
-EOH
-vim -c q
-
-cat ~/.original_vimrc > ~/.vimrc
-rm ~/.original_vimrc
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+# For example, we just use `~/.cache/dein` as installation directory
+sh ./installer.sh ~/.cache/dein
