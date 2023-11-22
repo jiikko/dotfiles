aa3e9897a2d7b04d3d85402b6dddd1e51b499f03
jiikko <n905i.1214@gmail.com>
2023-11-22 13:18:24 +0900

    symlink ignore global

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # set rc limlink
-for file in gemrc screenrc bashrc bash_profile gvimrc zshrc rspec gitconfig pryrc zlogin railsrc ideavimrc; do
+for file in gemrc screenrc bashrc bash_profile gvimrc zshrc rspec gitconfig pryrc zlogin railsrc ideavimrc gitignore_global; do
   echo 'making symlink' _$file '->' ~/.$file
   ln -s -F ~/dotfiles/_$file ~/.$file
 done
