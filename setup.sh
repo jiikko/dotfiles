b12b55f39e72110745649e05034e076d99acc085
jiikko <n905i.1214@gmail.com>
2025-07-20 15:51:57 +0900

    setup.shで失敗したら途中で止める

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# shellcheck disable=SC3040
+set -o pipefail
+
 # set rc limlink
 for file in gemrc screenrc bashrc bash_profile gvimrc zshrc rspec gitconfig pryrc zlogin railsrc ideavimrc gitignore_global; do
   echo 'making symlink' _$file '->' ~/.$file
